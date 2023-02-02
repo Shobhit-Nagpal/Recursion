c076aec4c0e698eea4db7de808e0f87453a209fe
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-02-02 11:02:35 +0530

    Uploaded fibs.js in Assignment 1

---
+++ Assignment-1/fibs.js
@@ -0,0 +1,18 @@
+const fibs = (num) => {
+    let fibsArr = [];
+    const first = 0;
+    const second = 1;
+
+    fibsArr.push(first);
+    fibsArr.push(second);
+
+    //num - 2 as we already added 2 elements in the array before and need 6 more iterations to get 8 elements in the array
+    for (let i = 0 ; i < num - 2 ; i++) {
+        let nextElement = fibsArr[i] + fibsArr[i+1];
+        fibsArr.push(nextElement);
+    }
+
+    console.log(fibsArr);
+}
+
+fibs(8);
